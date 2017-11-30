bcc03ee7a002ed775be1744fad951a3d6da97488
Conner Ardman <ardmanc@uw.edu>
2017-11-30 13:44:53 -0800

    Set stringsasfactors false

--- data.R
+++ data.R
@@ -5,8 +5,8 @@ library(RCurl)
 my.file <- getURL('https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/fatal-police-shootings-data.csv', ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
 # Use textConnection() to open a “connection” with the string, much like you would open a connection with a file on your hard drive in order to read it
 # Once you have a connection, read it in as a csv
-raw.data <- read.csv(textConnection(my.file), header=T)
-head(raw.data)
+raw.data <- read.csv(textConnection(my.file), header=T, stringsAsFactors = FALSE)
+# head(raw.data)
 
 
 # Use this in your files by calling source on this file name
