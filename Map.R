f5b9e2ee9def935d8447092e64a739f648b0eeb9
Conner Ardman <ardmanc@uw.edu>
2017-12-06 23:49:27 -0800

    Update CSS

--- Map.R
+++ Map.R
@@ -1,15 +1,17 @@
 library(dplyr)
 library(plotly)
 
-#TODO: style, comments, colorbar for counties, margins/sizing, conclusion info (mention excluded rows), DC?, name of panel
+#TODO: , colorbar for counties, conclusion info
 
 # Create a map of the shootings in each county of a given state. 
 # If Per is TRUE then use the data per 100,000 people
 showCounty <- function(state, per) {
   state.lower <- tolower(state)
-  city.counts <- raw.data %>% group_by(city, state) %>% summarize(count = n())
+  city.counts <- raw.data %>% group_by(city, state) %>% 
+    summarize(count = n())
   
   city.lat.long <- read.csv('uscitiesv1.3.csv', stringsAsFactors = FALSE)
+  
   # NOTE this is excluding some rows that don't match the city database. 
   # These are mostly very small towns
   city <- inner_join(city.counts, city.lat.long, by = c("city" = "city", "state" = "state_id"))
@@ -18,6 +20,7 @@ showCounty <- function(state, per) {
     summarize(count = sum(count), adjusted = (sum(count) / sum(population)) * 100000) %>% 
     mutate(lower_county_name = tolower(county_name), state_name = tolower(state_name))
   
+  # Get information about USA counties
   counties <- map_data("county")
   
   county.data <- left_join(counties, county, by = c("subregion" = "lower_county_name", "region" = "state_name"))
@@ -36,24 +39,23 @@ showCounty <- function(state, per) {
     title <- paste("Police Shootings By County:", state)
   }
   
+  # Render the plot
   county.data %>%
     group_by(group) %>%
-    plot_ly(x = ~long, y = ~lat, color = ~colorcat, colors = "Reds")  %>% 
-    add_polygons(line = list(width = 0.4)) %>% 
+    plot_ly(x = ~long, y = ~lat, color = ~colorcat, colors = "Reds", hoverinfo = "none")  %>% 
+    add_polygons(line = list(width = 0.4), hoverinfo = "none") %>% 
     add_polygons(
       fillcolor = 'transparent',
       line = list(color = 'black', width = 0.5),
-      showlegend = FALSE
-    ) %>%
+      showlegend = FALSE, hoverinfo = "none")  %>%
     add_trace(type = "scatter", opacity = 0, mode = "markers",
               text = ~paste0(gsub("(?<=\\b)([a-z])", "\\U\\1", subregion, perl=TRUE),
-                             "\n","Shootings: ", count, "\nPer 100,000: ", round(adjusted)), hoverinfo = "text") %>%
-    layout(autosize = F,
-           title = title, paper_bgcolor = "#4e5d6c", titlefont = titlefont, margin = list(t = "110"),    
-           xaxis = list(title = "", showgrid = FALSE,
-                        zeroline = FALSE, showticklabels = FALSE),
-           yaxis = list(title = "", showgrid = FALSE,
-                        zeroline = FALSE, showticklabels = FALSE), showlegend = FALSE) 
+                             "\n","Shootings: ", count, "\nPer 100,000: ", 
+                             round(adjusted)), hoverinfo = "text") %>%
+    layout(autosize = F, title = title, paper_bgcolor = "#4e5d6c", titlefont = titlefont, 
+           margin = list(t = "110"), xaxis = list(title = "", showgrid = FALSE,
+                                                  zeroline = FALSE, showticklabels = FALSE), yaxis = list(title = "", showgrid = FALSE,
+                                                                                                          zeroline = FALSE, showticklabels = FALSE), showlegend = FALSE)
 }
 
 
@@ -64,16 +66,20 @@ showState <- function(mill) {
   g <- list(scope = 'usa', projection = list(type = 'albers usa'))
   titlefont <- list(color = "white")
   output.data <- pop.and.shooting.data %>% mutate(data = total_by_state / pop_estimate_2016 * 1000000)
-  text <- ~paste(full_state_name,"\nTotal Shootings: ", total_by_state, "\nShootings Per Million People: ", round(data))
+  text <- ~paste(full_state_name,"\nTotal Shootings: ", 
+                 total_by_state, "\nShootings Per Million People: ", round(data))
   
   interest <- ~total_by_state
   if (mill) {
     interest <- ~data
   }
+  
   # Create a map of the police shootings per million people by state
   plot_geo(output.data, locationmode = 'USA-states') %>%
-    add_trace(z = interest, text = text, locations = ~state, color = interest, colors = 'Reds', hoverinfo = 'text') %>%
-    colorbar(title = "Shootings", titlefont = titlefont, tickfont = titlefont, tickcolor = "white") %>%
+    add_trace(z = interest, text = text, locations = ~state, 
+              color = interest, colors = 'Reds', hoverinfo = 'text') %>%
+    colorbar(title = "Shootings", titlefont = titlefont, 
+             tickfont = titlefont, tickcolor = "white") %>%
     layout(title = 'Police Shootings Per Million People', geo = g, autosize = F,
            paper_bgcolor = "#4e5d6c", titlefont = titlefont, margin = list(t = "110"))    
 }
