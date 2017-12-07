d41b1dfa5a9c0d88338e847d3ef3c83a572c6086
Conner Ardman <ardmanc@uw.edu>
2017-12-07 12:58:26 -0800

    Update Readme Solutions

--- ui.R
+++ ui.R
@@ -177,25 +177,25 @@ shinyUI(navbarPage("Police Shootings", theme = shinytheme("superhero"), selected
            fluidRow(
              column(width = 10, h1("What Does This Data Suggest For Federal Legislators?"),
                     p("Given that the Police Shooting issue is spread across all states, impacts every part of the population, and the initial solution 
-                             of using body cameras on police officers has had little effect thus far, there remains to be a huge improvement.", style = "background-color:#4e5d6c; padding:10px"),
+                      of using body cameras on police officers has had little effect thus far, there remains to be a huge improvement.", style = "background-color:#4e5d6c; padding:10px"),
                     p("When body cameras are used, the video recordings belong to the government, 
-                             and they are kept private. Many of the high profile police shootings were high-profile
-                             because a bystander recorded the incident on their phone and posted it online. With body cameras, the recordings will never be viral, 
-                             and the truth of the incident is kept behind red tape. We propose that body cameras continue to be used, but that the data be made public.
-                             
-                             For example, just today, a police officer was convicted for 20 years
-                            in jail for shooting an unarmed, fleeing, black male. 
-                             There was a viral video in this case that demonstrates the police officer was acting out of line with the misbehavior of the victim.
-                             We need more videos like this, and more police officers to be punished based on body camera footage. ", br(), 
-                            a("More about the Walter Scott case", href= "https://www.npr.org/sections/thetwo-way/2017/12/07/569070544/former-s-c-officer-who-killed-walter-scott-will-be-sentenced-to-up-to-24-years?sc=tw"), 
-                            style = "background-color:#4e5d6c; padding:10px"),
+                      and they are kept private. Many of the high profile police shootings were high-profile
+                      because a bystander recorded the incident on their phone and posted it online. With body cameras, the recordings will never be viral, 
+                      and the truth of the incident is kept behind red tape. We propose that body cameras continue to be used, but that the data be made public.
+                      
+                      For example, just today, a police officer was convicted for 20 years
+                      in jail for shooting an unarmed, fleeing, black male. 
+                      There was a viral video in this case that demonstrates the police officer was acting out of line with the misbehavior of the victim.
+                      We need more videos like this, and more police officers to be punished based on body camera footage. ", br(), 
+                      a("More about the Walter Scott case", href= "https://www.npr.org/sections/thetwo-way/2017/12/07/569070544/former-s-c-officer-who-killed-walter-scott-will-be-sentenced-to-up-to-24-years?sc=tw"), 
+                      style = "background-color:#4e5d6c; padding:10px"),
                     
                     p("Another point we could suggest is that more data should be collected on whether the body camera was turned on or off.",br(),
                       a("More about this suggestion", href="https://news.vice.com/article/reports-suggest-body-cameras-are-only-effective-when-cops-cant-turn-them-off"),
                       style = "background-color:#4e5d6c; padding:10px"),
                     
                     p("Finally, we wanted to say that our thoughts are with the families of the victims of these police shootings, and we hope that legistlators will act quickly to come to a solution in this matter.", style = "background-color:#4e5d6c; padding:10px")
-  ))),
+                    ))),
   
   # Footer
   br(),
